3d74d4d64ec1c1f4cf5e2467c81fc0ceec8956f2
akihiro <ueyama0105@gmail.com>
2021-02-10 19:53:09 +0900

    処理の集約 カメラの動的切り替え機能追加

--- Assets/SubViewControl.cs
+++ Assets/SubViewControl.cs
@@ -2,10 +2,14 @@
 
 public class SubViewControl : MonoBehaviour
 {
+    [SerializeField] private Renderer render;
+
     [Header("左右角度 : マウスのスクロール")]
     [Header("上下移動 : QEキー")]
     [Header("前後左右移動 : WASDキー or 矢印キー")]
     [Header("VR内のWebカメラの設定")]
+
+    [SerializeField] private uint Webカメラ番号 = 0;
 
     [Range(-90.0f, 90.0f)]
     [SerializeField] private float 上下角度 = 0.0f;
@@ -14,6 +18,7 @@ public class SubViewControl : MonoBehaviour
     [SerializeField] private float 画角 = 40.0f;
 
     private Camera targetCamera;
+    private WebCamTexture webcam = null;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +26,31 @@ public class SubViewControl : MonoBehaviour
         targetCamera = GetComponent<Camera>();
     }
 
+    private void startWebCamera(uint num)
+    {
+        var devices = WebCamTexture.devices;
+        if (devices.Length > num)
+        {
+            var deviceName = devices[num].name;
+            if (webcam != null && webcam.deviceName != deviceName)
+            {
+                webcam.Stop();
+                webcam = null;
+            }
+            if (webcam == null)
+            {
+                webcam = new WebCamTexture(deviceName);
+                render.material.mainTexture = webcam;
+                webcam.Play();
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
+        startWebCamera(Webカメラ番号);
+
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             targetCamera.transform.position += transform.forward * 0.1f;
